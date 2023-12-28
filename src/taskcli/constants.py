fc423ec4c2348e23875b4fb4e5fc87995f1e4c0a
pioio <76569454+pioio@users.noreply.github.com>
2023-12-29 00:00:09 +0100

    docs

---
+++ src/taskcli/constants.py
@@ -0,0 +1,13 @@
+"""Constants/strings used more than one place in the code. or accessed from code and unit tests.
+
+Storing them here:
+- makes the module dependencies simpler,
+- will make it easier to change them in the future.
+- makes unit tests less fragile (they make use of constants here)
+
+When writing new unit test, instead of hardcoding shared
+strings in unit tests, consider putting them here.
+"""
+
+ARG_SHOW_HIDDEN_SHORT = "-H"
+HELP_TEXT_USE_H_TO_SHOW_HIDDEN = f"use {ARG_SHOW_HIDDEN_SHORT} to show (or run 'tt')"
